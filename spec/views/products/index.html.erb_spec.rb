24ca1bd01d4ba3d6544edc9586e164ee54fbca1e
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-27 02:25:04 +0000

    Automatically backed up by Learn

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 # require 'rails_helper'
 
 # RSpec.describe "products/index.html.erb", type: :view do
 #   pending "add some examples to (or delete) #{__FILE__}"
 # end
+=======
+require 'rails_helper'
+
+RSpec.describe "products/index.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+>>>>>>> 07b0b28f6e458c83c8c823d3c724ba55cf34392f
